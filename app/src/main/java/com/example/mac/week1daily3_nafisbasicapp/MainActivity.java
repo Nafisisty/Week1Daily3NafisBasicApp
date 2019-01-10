69f82bae76954b03e615fe19d86888f65f167d5d
Nafisisty <nafisisty@gmail.com>
2019-01-10 10:14:24 -0500

    Changed the hard coded string and linked them with strings.xml file.

--- app/src/main/java/com/example/mac/week1daily3_nafisbasicapp/MainActivity.java
+++ app/src/main/java/com/example/mac/week1daily3_nafisbasicapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.mac.week1daily3_nafisbasicapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
