be2746ddf986e76ac9f490664c6ac9b8ea7b6585
Nafisisty <nafisisty@gmail.com>
2019-01-09 19:27:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mac/week1daily3_nafisbasicapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.mac.week1daily3_nafisbasicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+//    private Button showBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textViewId);
+        editText = findViewById(R.id.editTextId);
+//        showBtn = findViewById(R.id.showBtnId);
+
+
+
+//        showBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                String typedText = editText.getText().toString();
+//                textView.setText(typedText);
+//
+//            }
+//        });
+    }
+
+    public void onClick(View view){
+
+        String typedText = editText.getText().toString();
+        textView.setText(typedText);
+    }
+}
